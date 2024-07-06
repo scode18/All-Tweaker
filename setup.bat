f59ad8065433e03057aa5fec30c0f8c439e52a52
Solomon <98618381+scode18@users.noreply.github.com>
2024-07-06 19:13:31 +0300

    ⚙️update update.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 @echo off && title Install All Tweaker && mode con: cols=100 lines=25 && color a
 
 Utils\busybox wget https://github.com/scode18/All-Tweaker/raw/main/update.bat
+Utils\busybox wget https://github.com/scode18/All-Tweaker/raw/main/tabs_data.py
 
 update.bat
