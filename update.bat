06ebaeec634405e765b84abc9adc12483a4bd90a
Solomon <98618381+scode18@users.noreply.github.com>
2024-07-18 10:55:53 +0300

    💥пофиксил баг

--- update.bat
+++ update.bat
@@ -30,8 +30,10 @@ Utils\busybox wget https://github.com/scode18/All-Tweaker/raw/main/All.Tweaker.p
 Utils\busybox wget https://github.com/scode18/All-Tweaker/raw/main/tabs.py
 
 Utils\\busybox wget https://github.com/scode18/All-Tweaker/raw/main/All.Tweaker.Start.bat
-Utils\\busybox wget https://github.com/scode18/All-Tweaker/raw/main/Utils/elevator.exe
-Utils\\busybox wget https://github.com/scode18/All-Tweaker/raw/main/Utils/launcher.exe
+cd Utils
+busybox wget https://github.com/scode18/All-Tweaker/raw/main/Utils/elevator.exe
+busybox wget https://github.com/scode18/All-Tweaker/raw/main/Utils/launcher.exe
+cd ..
 
 REM Распаковываем архив с помощью 7zip
 Utils\7za x tweaks.7z tweaks
